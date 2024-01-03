fee6917c5df459e853c7d3e4c21bb96e997b7e22
Tulasi Joshua Mbawini <mbawinitulasi12@gmail.com>
2024-01-03 07:36:02 +0000

    Feat: toggle styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import React, { useState } from 'react'
 import './App.css'
+import Toggle from './components/Toggle';
 
 const App = () => {
   const [isDark, setIsDark] = useState(false);
   return (
     <div className='App' data-theme={isDark ? "dark" : "light"}>
+      <Toggle
+        isChecked={isDark}
+        handleChange={() => setIsDark(!isDark)} 
+      />
       <h1 className='title'>Hello world</h1>
       <div className='box'>
         <h2>This is a box</h2>
